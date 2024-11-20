2fb262b6ce6b486408a12b3b2d0e747702d3b5b8
Jesús Pablo Medina García <jp.medina20@info.uas.edu.mx>
2024-11-19 21:04:32 -0700

    Plantilla ProductSeeder actualizada

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -12,29 +12,42 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        $titles = [
-            'Ejemplo de título',
+        $categories = [
+            1 => [ // ID de la categoría
+                'titles' => [
+                    // Títulos
+                ],
+                'descriptions' => [
+                    // Descripciones
+                ],
+            ],
+            2 => [ // ID de la categoría
+                'titles' => [
+                    // Títulos
+                ],
+                'descriptions' => [
+                    // Descripciones
+                ],
+            ],
         ];
 
-        $descriptions = [
-            'Ejemplo de descripción.',
-        ];
-
-        for ($i = 0; $i < 30; $i++) {
-            Product::create([
-                'title' => $titles[array_rand($titles)],
-                'description' => $descriptions[array_rand($descriptions)],
-                'condition' => ['new', 'used', 'reaconditioned'][array_rand(['new', 'used', 'reaconditioned'])],
-                'stock' => rand(10, 200),
-                'price' => rand(100, 5000),
-                'publication_type' => ['free', 'classic', 'premium'][array_rand(['free', 'classic', 'premium'])],
-                'warranty_type' => ['manufacturer', 'seller'][array_rand(['manufacturer', 'seller'])],
-                'warranty_duration' => rand(1, 24),
-                'warranty_duration_type' => ['days', 'months', 'years'][array_rand(['days', 'months', 'years'])],
-                'status' => ['draft', 'published'][array_rand(['draft', 'published'])],
-                'user_id' => rand(1, 9),
-                'category_id' => 2,
-            ]);
+        foreach ($categories as $categoryId => $data) {
+            for ($i = 0; $i < 30; $i++) { // 30 productos por categoría
+                Product::create([
+                    'title' => $data['titles'][array_rand($data['titles'])],
+                    'description' => $data['descriptions'][array_rand($data['descriptions'])],
+                    'condition' => ['new', 'used', 'reaconditioned'][array_rand(['new', 'used', 'reaconditioned'])],
+                    'stock' => rand(10, 200),
+                    'price' => rand(100, 5000),
+                    'publication_type' => ['free', 'classic', 'premium'][array_rand(['free', 'classic', 'premium'])],
+                    'warranty_type' => ['manufacturer', 'seller'][array_rand(['manufacturer', 'seller'])],
+                    'warranty_duration' => rand(1, 24),
+                    'warranty_duration_type' => ['days', 'months', 'years'][array_rand(['days', 'months', 'years'])],
+                    'status' => ['draft', 'published'][array_rand(['draft', 'published'])],
+                    'user_id' => rand(1, 9),
+                    'category_id' => $categoryId,
+                ]);
+            }
         }
     }
 }
